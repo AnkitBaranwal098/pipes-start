a143db31858b4a737adf310a905b5f6c196be442
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-11-07 00:55:11 +0530

    custom pipe demo

---
+++ src/app/shorten.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+// To make this class treated as pipe we need to use the pipe decorator and inside that pass the name which will take the pipe name which we can now use in template
+@Pipe({
+    name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform{
+
+    // Transform must always return something as pipe in that you put something in and we get something out
+    transform(value: any) {
+        if(value.length>10)
+            return value.substr(0,10) + ' ...'
+        return value;
+    }
+} 
+
+// Now to use this pipe we need to got to the app.module.ts and inside the declarations we add the ShortenPipe
